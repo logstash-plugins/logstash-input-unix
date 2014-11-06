5200541de4e907c427bb516d8097f5e8a05886f1
Suyog Rao <suyog.rao@gmail.com>
2014-11-06 12:57:35 -0800

    Added inline code asciidoc formatting

--- lib/logstash/inputs/unix.rb
+++ lib/logstash/inputs/unix.rb
@@ -5,7 +5,7 @@ require "socket"
 
 # Read events over a UNIX socket.
 #
-# Like stdin and file inputs, each event is assumed to be one line of text.
+# Like `stdin` and `file` inputs, each event is assumed to be one line of text.
 #
 # Can either accept connections from clients or connect to a server,
 # depending on `mode`.
