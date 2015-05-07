17e3075f4fea94b4e7f1090b97ff95311f4844aa
michael <michael@adience.com>
2015-05-07 19:21:20 +0300

    removed 'path' from event details

--- lib/logstash/inputs/unix.rb
+++ lib/logstash/inputs/unix.rb
@@ -91,7 +91,6 @@ class LogStash::Inputs::Unix < LogStash::Inputs::Base
         @codec.decode(buf) do |event|
           decorate(event)
           event["host"] = hostname
-          event["path"] = @path
           output_queue << event
         end
       end # loop do
