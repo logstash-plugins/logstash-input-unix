ad1d332a5877a96d5cbe13628c8c3a07a7c66edf
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-26 18:52:15 +0200

    add basic lookup test for the plugin

--- spec/inputs/unix_spec.rb
+++ spec/inputs/unix_spec.rb
@@ -1,1 +1,15 @@
-require "logstash/devutils/rspec/spec_helper"
+# encoding: utf-8
+require_relative "../spec_helper"
+require "stud/temporary"
+require "tempfile"
+
+describe LogStash::Inputs::Unix do
+
+  let(:tempfile)   { Tempfile.new("/tmp/foo") }
+
+  it "should register without errors" do
+    plugin = LogStash::Plugin.lookup("input", "unix").new({ "path" => tempfile.path, "force_unlink" => true })
+    expect { plugin.register }.to_not raise_error
+  end
+
+end
