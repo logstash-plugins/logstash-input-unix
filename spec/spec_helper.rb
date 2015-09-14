f61e6804d79e650e0658c9d03fa7702d004e20c5
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-09-14 16:57:24 +0200

    Make usage of the new stop api
    Make sure the client socket can be closed when the underneath socket has not more data and readpartial blocks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,33 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
 require 'logstash/inputs/unix'
+
+class UnixSocketHelper
+
+  attr_reader :path
+
+  def initialize
+    @socket = nil
+  end
+
+  def new_socket(path)
+    @path   = path
+    File.unlink if File.exists?(path) && File.socket?(path)
+    @socket = UNIXServer.new(path)
+    self
+  end
+
+  def loop(forever=false)
+    @thread = Thread.new do
+      s = @socket.accept
+      s.puts "hi" while forever
+    end
+    self
+  end
+
+  def close
+    @thread.kill
+    @socket.close
+    File.unlink(path)
+  end
+end
